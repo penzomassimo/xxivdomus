5e1e406ccfac3302c68af08498a4232ac6a00fbc
Massimo Penzo <massimopenzo@Massimos-MacBook-Pro.local>
2025-07-14 11:19:04 +0200

    Your commit message here

--- project/models/example/octorate_prenotazioni_clean.sql
+++ project/models/example/octorate_prenotazioni_clean.sql
@@ -5,7 +5,7 @@ select
 	u.data_creazione,
 	to_date(u.data_creazione, 'DD/MM/YYYY') as data_creazione_parsed,
 	extract(dow from to_date(u.data_creazione, 'DD/MM/YYYY')) as data_creazione_dow,
-	extract(month from to_date(u.data_creazione, 'DD/MM/YYYY')) as data_creazione_month,
+	extract(month from to_date(u.data_creazione, 'DD/MM/YYYY')) as data_creazione_month
 		
 from google_drive.octorate_prenotazioni_confermate_sheet_1 u
 
