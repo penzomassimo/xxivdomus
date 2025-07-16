a270600fbeb968ed2eddd68636667f7a7993efaf
Massimo Penzo <massimopenzo@Massimos-MacBook-Pro.local>
2025-07-16 19:16:40 +0200

    Your commit message here

---
+++ project/models/example/bank_account_monthly_balance.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='table') }}
+
+SELECT DISTINCT ON (DATE_TRUNC('month', w.data_operazione_parsed))
+  extract(month from w.data_operazione_parsed) as data_operazione_month,
+  w.data_operazione_parsed,
+  w.running_balance,
+  w.row_number
+from {{ ref('bank_account_extract') }} w
+ORDER BY DATE_TRUNC('month', w.data_operazione_parsed), w.row_number DESC;
