a76f00a3b128aa36d491c58a22ea95cc00076bc9
Massimo Penzo <massimopenzo@Massimos-MacBook-Pro.local>
2025-07-16 15:29:55 +0200

    Your commit message here

---
+++ project/models/example/bank_account_extract.sql
@@ -0,0 +1,22 @@
+
+{{ config(materialized='table') }}
+
+select
+	to_date(b.data_operazione, 'MM/DD/YY') as data_operazione_parsed,
+	extract(month from to_date(b.data_operazione, 'MM/DD/YY')) as data_operazione_month,
+	extract(year from to_date(b.data_operazione, 'MM/DD/YY')) as data_operazione_year,
+	
+	to_date(b.data_valuta, 'MM/DD/YY') as data_valuta_parsed,
+	extract(month from to_date(b.data_valuta, 'MM/DD/YY')) as data_valuta_month,
+	extract(year from to_date(b.data_valuta, 'MM/DD/YY')) as data_valuta_year,
+	
+	CASE
+  		WHEN b.eur LIKE '(%' THEN
+    	-- Negative value in parentheses
+    	-1 * CAST(REPLACE(REPLACE(TRIM(BOTH '()' FROM b.eur), '.', ''), ',', '.') AS NUMERIC)
+  		ELSE
+    	-- Positive value
+    	CAST(REPLACE(REPLACE(b.eur, '.', ''), ',', '.') AS NUMERIC)
+	END AS trx_amount
+		
+from {{ source('octorate_prenotazioni', 'bank_account_sheet_1') }} as b
