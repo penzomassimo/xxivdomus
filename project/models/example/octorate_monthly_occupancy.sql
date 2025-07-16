f11d94ec86a63b9280df986b7f58c83c88c70810
Massimo Penzo <massimopenzo@Massimos-MacBook-Pro.local>
2025-07-16 10:15:53 +0200

    Your commit message here

--- project/models/example/octorate_monthly_occupancy.sql
+++ project/models/example/octorate_monthly_occupancy.sql
@@ -9,7 +9,8 @@ select
 	count(o.data_month) * 6 as total_nights_in_month,
 	(count(o.data_month) * 6) - (sum(o.superior_available) + sum(o.suite_available) + sum(o.deluxe_available) + sum(o.single_available)) as total_nights_sold,
 	count(o.data_month) as days_in_month,
-	((count(o.data_month) * 6::numeric) - (sum(o.superior_available) + sum(o.suite_available) + sum(o.deluxe_available) + sum(o.single_available))) / (count(o.data_month) * 6) as occupancy_month
+	((count(o.data_month) * 6::numeric) - (sum(o.superior_available) + sum(o.suite_available) + sum(o.deluxe_available) + sum(o.single_available))) / (count(o.data_month) * 6) as occupancy_month,
+	o.data_month as data_month
 	
 from {{ ref('octorate_occupancy') }} o
 group by o.data_month
