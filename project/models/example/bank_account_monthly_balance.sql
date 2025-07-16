650d400708598c3ecced5df4f783149c0e34647e
Massimo Penzo <massimopenzo@Massimos-MacBook-Pro.local>
2025-07-16 19:29:38 +0200

    Your commit message here

--- project/models/example/bank_account_monthly_balance.sql
+++ project/models/example/bank_account_monthly_balance.sql
@@ -2,6 +2,7 @@
 
 SELECT DISTINCT ON (DATE_TRUNC('month', w.data_operazione_parsed))
   extract(month from w.data_operazione_parsed) as data_operazione_month,
+  extract(year from w.data_operazione_parsed) as data_operazione_year,
   w.data_operazione_parsed,
   w.running_balance,
   w.row_number
