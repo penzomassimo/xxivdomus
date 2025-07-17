b73a296a2bd3a0e46ead5114d9b1a33df7e46ffd
Massimo Penzo <massimopenzo@Massimos-MacBook-Pro.local>
2025-07-17 10:13:56 +0200

    Your commit message here

--- project/models/example/monthly_cashflow.sql
+++ project/models/example/monthly_cashflow.sql
@@ -1,12 +1,13 @@
 
 {{ config(materialized='table') }}
 select 
-b.data_operazione_month,
+	b.data_operazione_month,
+	b.data_operazione_year,
 	SUM(CASE WHEN b.trx_amount > 0 THEN b.trx_amount ELSE 0 END) AS cash_inflows,
 	SUM(CASE WHEN b.trx_amount < 0 THEN b.trx_amount ELSE 0 END) AS cash_outflows,
 	sum(b.trx_amount) as net_cashflow,
 	sum(CASE WHEN b.trx_amount > 0 THEN 1 ELSE 0 END) AS count_cash_inflows,
 	sum(CASE WHEN b.trx_amount < 0 THEN 1 ELSE 0 END) AS count_cash_outflows	
 from {{ ref('bank_account_extract') }} b
-group by b.data_operazione_month
+group by b.data_operazione_month, b.data_operazione_year
 order by b.data_operazione_month
